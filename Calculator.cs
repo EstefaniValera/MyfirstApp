b158aacec74c89eccc541cf7e64a7f5a8544450a
EstefaniValera <estefaniparedes@capitole-consulting.com>
2021-06-25 11:08:03 +0200

    commit 1

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System; // salen en gris porque no los estas usando ; importa todas las clases que tienes en ese paquete
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; //ojo si lo subes s git, no subir codigo de mas
+
+// namespace = paquetes donde voy a contener las clase
+// en -net no puedo importar una clase - puedo importa namespace
+
+namespace MyFirstApp
+{
+    public class Calculator // poner publica
+    {
+    // por defecto  c# no hace las clases publicas
+    //camel casing; la nomenclatura, es un formato que utilizas ; uSAR LA NOMENCLATURA CORRECTA camel casing-pascal casing
+    public int Add(int num1, int num2) // calidad de codigo, te lo valida el sonarqube si cumple la nomenclatura del lenguaje
+        {
+            return num1 + num2;
+        }
+
+    public int Substract(int num1, int num2) // calidad de codigo, te lo valida el sonarqube si cumple la nomenclatura del lenguaje
+        {
+            return num1 - num2;
+        }
+
+    public int Multiply(int num1, int num2) // calidad de codigo, te lo valida el sonarqube si cumple la nomenclatura del lenguaje
+        {
+            return num1 * num2;
+        }
+
+    public int Divide(int num1, int num2) // calidad de codigo, te lo valida el sonarqube si cumple la nomenclatura del lenguaje
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ for.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main()
+	{
+		/*for (int counter = 0; counter < 10; counter++)
+		{
+			Console.WriteLine($"Hello World! The counter is {counter}");
+		}
+
+		int counterNames = 0;
+		while (counterNames < 10)
+		{
+			Console.WriteLine($"Hello World! The counter is {counterNames}");
+			counterNames++;
+		}*/
+		/*ALT ; con bloques*/
+		/*CTRL+ALT+Pulsar para elegir varias lineas de codigocounterNames*/
+		/*Puede buscar y reemplazar texto en el editor de Visual Studio mediante Buscar y reemplazar (Ctrl+F o Ctrl+H) o Find/Replace in Files (Buscar/Reemplazar en archivos) (Ctrl+Mayús+F o Ctrl+Mayús+H).*/
+		//int counterOne = 0;
+		/*do
+		{
+			Console.WriteLine($"Hello World! The counter is {counterOne}");
+			counterOne++;
+		} while (counterOne < 10);
+
+		for (int row = 1; row < 11; row++)
+		{
+			Console.WriteLine($"The row is {row}");
+		}
+
+		for (int row = 1; row <5; row++)
+		{
+			for (char column = 'a'; column < 'h'; column++)
+			{
+				Console.WriteLine($"The cell is ({row}, {column})");
+			}
+		}
+
+		int i;
+		int j = 3;
+		for (i = 0, Console.WriteLine($"Start: i={i}, j={j}"); i < j; i++, j--, Console.WriteLine($"Step: i={i}, j={j}"))
+		{
+			Console.WriteLine("---");
+		}
+
+		
+		Console.WriteLine("///////////////");
+
+		Class1 class1 = new Class1();
+
+		int i;
+		int j = 3;
+
+        for (i=0, class1.startLoop(i,j); i < j; i++, j--, class1.stepLoop(i,j))
+        {
+			Console.WriteLine("---");
+		}*/
+		Console.WriteLine("///////////////");
+		int i;
+		int j = 3;
+		Class1 class1 = new Class1();
+		for (i = 0, class1.startLoop(i, j); i < j; i++, j--, class1.stepLoop(i, j))
+		{
+			Console.WriteLine("---");
+		}
+		Console.WriteLine("///////////////");
+		var fibNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13 };
+        foreach (int element in fibNumbers)
+        {
+			Console.Write($"{element}");
+        }
+
+	}
+
+	public void startLoop(int i, int j)
+    {
+		Console.WriteLine($"Start: i={i}, j={j}");
+	}
+	public void stepLoop(int i, int j) => Console.WriteLine($"Step: i={i}, j={j}");
+
+	
+}
